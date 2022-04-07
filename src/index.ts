ffbcea06af50b6f9d09d0fd46a2009f34f3c88ff
Dan Edington <dan2505@hotmail.com>
2022-04-07 23:48:41 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,229 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import dat from 'dat.gui';
+import './noise.js';
+
+(async () => {
+	const config = {
+		curtain: {
+			width: 1.5,
+			height: 2,
+			resolution: 100,
+			roughness: 1,
+			metalness: 0.11,
+			folds: 5,
+			foldDepth: 0.025,
+		},
+		lights: {
+			ambient: {
+				color: 0xfff8e7,
+				intensity: 3,
+			},
+			mainLight: {
+				color: 0xfff8e7,
+				intensity: 27,
+				position: {
+					x: 1,
+					y: 2.7,
+					z: -2.64,
+				},
+			},
+			secondaryLight: {
+				color: 0xfff8e7,
+				intensity: 7,
+				position: {
+					x: -3,
+					y: -2.75,
+					z: -4.2,
+				},
+			},
+		},
+	};
+
+	const gui = new dat.GUI();
+
+	const scene = new THREE.Scene();
+
+	const camera = new THREE.PerspectiveCamera(
+		75,
+		window.innerWidth / window.innerHeight,
+		0.1,
+		1000
+	);
+	camera.position.z = -2;
+
+	const renderer = new THREE.WebGLRenderer({ antialias: true });
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.outputEncoding = THREE.sRGBEncoding;
+	renderer.toneMapping = THREE.ACESFilmicToneMapping;
+	renderer.physicallyCorrectLights = true;
+	renderer.shadowMap.enabled = true;
+	renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+	renderer.setClearColor(0xc9c9c9);
+	document.body.appendChild(renderer.domElement);
+
+	const orbit = new OrbitControls(camera, renderer.domElement);
+
+	const curtain = new THREE.Mesh(
+		new THREE.PlaneBufferGeometry(
+			config.curtain.width,
+			config.curtain.height,
+			config.curtain.resolution,
+			config.curtain.resolution
+		),
+		new THREE.MeshPhysicalMaterial({ color: 0x363636, side: THREE.DoubleSide })
+	);
+
+	curtain.castShadow = true;
+	curtain.receiveShadow = true;
+	curtain.rotateY(Math.PI);
+
+	const curtainFolder = gui.addFolder('Curtain');
+
+	const l = config.lights;
+
+	const ambientLight = new THREE.AmbientLight(
+		l.ambient.color,
+		l.ambient.intensity
+	);
+
+	const mainLight = new THREE.PointLight(
+		l.mainLight.color,
+		l.mainLight.intensity
+	);
+	mainLight.position.set(
+		l.mainLight.position.x,
+		l.mainLight.position.y,
+		l.mainLight.position.z
+	);
+	const mainLightHelper = new THREE.PointLightHelper(mainLight);
+	scene.add(mainLightHelper);
+	mainLight.castShadow = true;
+	mainLight.shadow.bias = -0.001;
+
+	const secondaryLight = new THREE.PointLight(
+		l.secondaryLight.color,
+		l.secondaryLight.intensity
+	);
+	secondaryLight.position.set(
+		l.secondaryLight.position.x,
+		l.secondaryLight.position.y,
+		l.secondaryLight.position.z
+	);
+	const secondaryLightHelper = new THREE.PointLightHelper(secondaryLight);
+	scene.add(secondaryLightHelper);
+	secondaryLight.castShadow = true;
+	secondaryLight.shadow.bias = -0.001;
+
+	const folderMainLight = gui.addFolder('Main Light');
+	folderMainLight.add(mainLight, 'intensity', 0, 100);
+	folderMainLight.add(mainLight.position, 'x', -5, 5);
+	folderMainLight.add(mainLight.position, 'y', -5, 5);
+	folderMainLight.add(mainLight.position, 'z', -5, 5);
+
+	const folderSecondaryLight = gui.addFolder('Secondary Light');
+	folderSecondaryLight.add(secondaryLight, 'intensity', 0, 100);
+	folderSecondaryLight.add(secondaryLight.position, 'x', -5, 5);
+	folderSecondaryLight.add(secondaryLight.position, 'y', -5, 5);
+	folderSecondaryLight.add(secondaryLight.position, 'z', -5, 5);
+
+	const folderAmbientLight = gui.addFolder('Ambient Light');
+	folderAmbientLight.add(ambientLight, 'intensity', 0, 100);
+
+	scene.add(curtain, mainLight, ambientLight, secondaryLight);
+
+	const applySin = (mesh: THREE.Mesh, folds: number, foldDepth: number) => {
+		const geometry = mesh.geometry;
+		const positionAttribute = geometry.getAttribute('position');
+		const vertex = new THREE.Vector3();
+		for (let i = 0; i < positionAttribute.count; i++) {
+			vertex.fromBufferAttribute(positionAttribute, i);
+			const angle = Math.PI * (vertex.x * 2);
+			const wave1 = foldDepth * Math.sin(angle * folds);
+			const wave2 = 0.01 * Math.sin(angle * 8);
+			const wave = wave1 + wave2;
+			vertex.z = wave;
+			positionAttribute.setZ(i, vertex.z);
+		}
+		positionAttribute.needsUpdate = true;
+	};
+
+	const applyNoise = (
+		mesh: THREE.Mesh,
+		scale: number,
+		axes: string = 'xyz'
+	) => {
+		const geometry = mesh.geometry;
+		const positionAttribute = geometry.getAttribute('position');
+		const vertex = new THREE.Vector3();
+		let alterX,
+			alterY,
+			alterZ = false;
+		axes
+			.toLowerCase()
+			.split('')
+			.forEach((axis) => {
+				if (axis === 'x') alterX = true;
+				if (axis === 'y') alterY = true;
+				if (axis === 'z') alterZ = true;
+			});
+		for (let i = 0; i < positionAttribute.count; i++) {
+			vertex.fromBufferAttribute(positionAttribute, i);
+			//@ts-ignore
+			const n = noise.perlin3(vertex.x, vertex.y, vertex.z) * scale;
+			vertex.x += alterX ? n : 0;
+			vertex.y += alterY ? n : 0;
+			vertex.z += alterZ ? n : 0;
+			positionAttribute.setXYZ(i, vertex.x, vertex.y, vertex.z);
+		}
+		positionAttribute.needsUpdate = true;
+	};
+
+	applySin(curtain, config.curtain.folds, config.curtain.foldDepth);
+	applyNoise(curtain, 0.05);
+	applyNoise(curtain, 0.01, 'xy');
+	applyNoise(curtain, 0.1, 'z');
+
+	curtain.geometry.computeVertexNormals();
+
+	const render = () => {
+		requestAnimationFrame(render);
+		orbit.update();
+		renderer.render(scene, camera);
+	};
+
+	const textureLoader = new THREE.TextureLoader();
+
+	const texture = await textureLoader.load('./fabric_pattern_07_col_1_1k.png');
+	texture.wrapS = THREE.RepeatWrapping;
+	texture.wrapT = THREE.RepeatWrapping;
+
+	const roughMap = await textureLoader.load('./fabric_pattern_07_rough_1k.png');
+	roughMap.wrapS = THREE.RepeatWrapping;
+	roughMap.wrapT = THREE.RepeatWrapping;
+
+	const normalMap = await textureLoader.load(
+		'./fabric_pattern_07_nor_gl_1k.png'
+	);
+	normalMap.wrapS = THREE.RepeatWrapping;
+	normalMap.wrapT = THREE.RepeatWrapping;
+
+	const aoMap = await textureLoader.load('./fabric_pattern_07_ao_1k.png');
+	aoMap.wrapS = THREE.RepeatWrapping;
+	aoMap.wrapT = THREE.RepeatWrapping;
+
+	const ratio = config.curtain.width / config.curtain.height;
+	const repeats = 3;
+	texture.repeat = new THREE.Vector2(repeats, repeats * (1 + ratio));
+	curtain.material.map = texture;
+	curtain.material.aoMap = aoMap;
+	curtain.material.normalMap = normalMap;
+	curtain.material.roughnessMap = roughMap;
+	curtain.material.roughness = config.curtain.roughness;
+	curtain.material.metalness = config.curtain.metalness;
+	curtain.material.reflectivity = 0;
+	curtainFolder.add(curtain.material, 'roughness', 0, 1);
+	curtainFolder.add(curtain.material, 'metalness', 0, 1);
+
+	render();
+})();
