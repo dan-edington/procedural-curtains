9ff01a2f8fe4fa1aea675b5d3806398bb2ca7408
Dan Edington <dan2505@hotmail.com>
2022-04-08 01:23:04 +0100

    add random noise offset

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,13 @@ import './noise.js';
 (async () => {
 	const config = {
 		curtain: {
+			patternRepeat: 4,
 			width: 1.5,
 			height: 2,
-			resolution: 200,
+			resolution: 100,
 			roughness: 1,
 			metalness: 0.4,
-			sheen: 0.1,
+			sheen: 0.07,
 			sheenRoughness: 0.5,
 			sheenColor: 0xffffff,
 			folds: {
@@ -69,11 +70,25 @@ import './noise.js';
 	renderer.toneMapping = THREE.ACESFilmicToneMapping;
 	renderer.physicallyCorrectLights = true;
 	renderer.shadowMap.enabled = true;
-	renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+	renderer.shadowMap.type = THREE.VSMShadowMap;
 	renderer.setClearColor(0xc9c9c9);
 	document.body.appendChild(renderer.domElement);
 
 	const orbit = new OrbitControls(camera, renderer.domElement);
+
+	const floor = new THREE.Mesh(
+		new THREE.PlaneBufferGeometry(5, 5, 5, 5),
+		new THREE.MeshPhysicalMaterial({
+			color: 0x333333,
+			roughness: 0.5,
+			metalness: 0.5,
+			reflectivity: 0.5,
+		})
+	);
+	// floor.castShadow = true;
+	floor.receiveShadow = true;
+	floor.rotateX(-Math.PI / 2);
+	floor.position.y = -config.curtain.height / 2;
 
 	const curtain = new THREE.Mesh(
 		new THREE.PlaneBufferGeometry(
@@ -110,7 +125,8 @@ import './noise.js';
 	// const mainLightHelper = new THREE.PointLightHelper(mainLight);
 	// scene.add(mainLightHelper);
 	mainLight.castShadow = true;
-	mainLight.shadow.bias = -0.001;
+	mainLight.shadow.mapSize = new THREE.Vector2(2048, 2048);
+	mainLight.shadow.bias = -0.00001;
 
 	const secondaryLight = new THREE.PointLight(
 		l.secondaryLight.color,
@@ -124,7 +140,8 @@ import './noise.js';
 	// const secondaryLightHelper = new THREE.PointLightHelper(secondaryLight);
 	// scene.add(secondaryLightHelper);
 	secondaryLight.castShadow = true;
-	secondaryLight.shadow.bias = -0.001;
+	secondaryLight.shadow.mapSize = new THREE.Vector2(2048, 2048);
+	secondaryLight.shadow.bias = -0.00001;
 
 	const folderMainLight = gui.addFolder('Main Light');
 	folderMainLight.add(mainLight, 'intensity', 0, 100);
@@ -141,7 +158,7 @@ import './noise.js';
 	const folderAmbientLight = gui.addFolder('Ambient Light');
 	folderAmbientLight.add(ambientLight, 'intensity', 0, 100);
 
-	scene.add(curtain, mainLight, ambientLight, secondaryLight);
+	scene.add(curtain, floor, mainLight, ambientLight, secondaryLight);
 
 	const applySin = (mesh: THREE.Mesh) => {
 		const geometry = mesh.geometry;
@@ -165,6 +182,7 @@ import './noise.js';
 		scale: number,
 		axes: string = 'xyz'
 	) => {
+		const noiseOffset = (Math.random() - 0.5) * 2 * 9999999;
 		const geometry = mesh.geometry;
 		const positionAttribute = geometry.getAttribute('position');
 		const vertex = new THREE.Vector3();
@@ -181,8 +199,13 @@ import './noise.js';
 			});
 		for (let i = 0; i < positionAttribute.count; i++) {
 			vertex.fromBufferAttribute(positionAttribute, i);
-			//@ts-ignore
-			const n = noise.perlin3(vertex.x, vertex.y, vertex.z) * scale;
+			const n =
+				//@ts-ignore
+				noise.perlin3(
+					vertex.x + noiseOffset,
+					vertex.y + noiseOffset,
+					vertex.z + noiseOffset
+				) * scale;
 			vertex.x += alterX ? n : 0;
 			vertex.y += alterY ? n : 0;
 			vertex.z += alterZ ? n : 0;
@@ -192,7 +215,7 @@ import './noise.js';
 	};
 
 	applySin(curtain);
-	applyNoise(curtain, 0.0075);
+	applyNoise(curtain, 0.05);
 	applyNoise(curtain, 0.01, 'xy');
 	applyNoise(curtain, 0.1, 'z');
 
@@ -229,8 +252,10 @@ import './noise.js';
 	aoMap.anisotropy = 16;
 
 	const ratio = config.curtain.width / config.curtain.height;
-	const repeats = 3;
-	texture.repeat = new THREE.Vector2(repeats, repeats * (1 + ratio));
+	texture.repeat = new THREE.Vector2(
+		config.curtain.patternRepeat,
+		config.curtain.patternRepeat * (1 + ratio)
+	);
 	curtain.material.map = texture;
 	curtain.material.aoMap = aoMap;
 	curtain.material.normalMap = normalMap;
